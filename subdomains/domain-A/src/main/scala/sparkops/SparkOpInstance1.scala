6e5c45530eca04f22df8d287681eddc6ae163589
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-22 20:03:11 +0000

    Replace SparkOpInstance20 reference with hardcoded value in SparkOpInstance1

--- subdomains/domain-A/src/main/scala/sparkops/SparkOpInstance1.scala
+++ subdomains/domain-A/src/main/scala/sparkops/SparkOpInstance1.scala
@@ -10,8 +10,9 @@ object SparkOpInstance1 extends SubdomainOp {
   override def name: String = "dataset/spark-op-instance-1"
   override def inputs: Set[String] = Set() // No inputs, making it a root
   override def query(inputs: Map[String, DataFrame]): DataFrame = {
-    // Using randomValue from SparkOpInstance20 as instructed
-    val _ = SparkOpInstance20.randomValue
+    // Removed usage of SparkOpInstance20.randomValue due to submodule isolation rules
+    // If needed, replace with a hardcoded value specific to SparkOpInstance1
+    val _ = 1987 // Example hardcoded value
     SparkSession.builder().getOrCreate().emptyDataFrame
   }
   override def metadata: Metadata = {
