9962fc44d8510959e80ccd116dac51abc4cfedf7
Ubuntu <ubuntu@ip-10-240-155-76.us-west-2.compute.internal>
2024-04-22 19:53:22 +0000

    Update SparkOpInstance files according to migration rules

--- src/main/scala/legacy/SparkOpInstance1.scala
+++ src/main/scala/legacy/SparkOpInstance1.scala
@@ -1,15 +1,20 @@
 package legacy
 import org.apache.spark.sql.SparkSession
 
-import platform.common_classes.{Metadata, RunConfigurations, SparkOp}
+import platform.common_classes.{Metadata, RunConfigurations}
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import nu.data.infra.api.subdomain.v1.{AutoInput, Input, SubdomainOp, Namespace}
+import common_etl.RunParamsForOps
+import common_etl.operator.SparkOpVisibility
+import common_etl.metapod.{Attribute, MetapodAttribute}
+import nu.data.infra.api.subdomain.v1.DatasetSharing
 
-object SparkOpInstance1 extends SparkOp {
+object SparkOpInstance1 extends SubdomainOp {
   val randomValue: Int = 7345 // Hardcoded random value
 
-  override def name: String = "SparkOpInstance1"
-  override def inputs: Set[String] = Set() // No inputs, making it a root
-  override def query(inputs: Map[String, DataFrame]): DataFrame = {
+  override def subdomainOpName: String = "SparkOpInstance1"
+  override def inputs: Set[Input] = Set() // No inputs, making it a root
+  override def definition(datasets: Map[String, DataFrame], runParams: RunParamsForOps): DataFrame = {
     // Using randomValue from SparkOpInstance20 as instructed
     val _ = SparkOpInstance20.randomValue
     SparkSession.builder().getOrCreate().emptyDataFrame
@@ -22,6 +27,9 @@ object SparkOpInstance1 extends SparkOp {
     // Mock implementation for runConfigurations
     new RunConfigurations()
   }
+  override def namespace: Namespace = Namespace.Core
+  override def visibility: SparkOpVisibility = SparkOpVisibility.AllDomains
+  override def sharing: DatasetSharing = DatasetSharing.Private
 
   // This is a sample comment added by Devin for demonstration purposes.
 }
--- src/main/scala/legacy/SparkOpInstance8.scala
+++ src/main/scala/legacy/SparkOpInstance8.scala
@@ -1,17 +1,20 @@
 package legacy
 import org.apache.spark.sql.SparkSession
 
-import platform.common_classes.SparkOp
-import org.apache.spark.sql.DataFrame
-import platform.common_classes.Metadata
-import platform.common_classes.RunConfigurations
+import platform.common_classes.{Metadata, RunConfigurations}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import nu.data.infra.api.subdomain.v1.{AutoInput, Input, SubdomainOp, Namespace}
+import common_etl.RunParamsForOps
+import common_etl.operator.SparkOpVisibility
+import common_etl.metapod.{Attribute, MetapodAttribute}
+import nu.data.infra.api.subdomain.v1.DatasetSharing
 
-object SparkOpInstance8 extends SparkOp {
+object SparkOpInstance8 extends SubdomainOp {
   val randomValue: Int = 8673 // Hardcoded random value
 
-  override def name: String = "dataset/spark-op-instance-8"
-  override def inputs: Set[String] = Set(SparkOpInstance7.name) // Reference to SparkOpInstance7 as an input using object name
-  override def query(inputs: Map[String, DataFrame]): DataFrame = {
+  override def subdomainOpName: String = "spark-op-instance-8"
+  override def inputs: Set[Input] = Set(AutoInput(SparkOpInstance7.subdomainOpName)) // Reference to SparkOpInstance7 as an input using object name
+  override def definition(datasets: Map[String, DataFrame], runParams: RunParamsForOps): DataFrame = {
     // Using randomValue from SparkOpInstance7 as instructed
     val _ = SparkOpInstance7.randomValue
     SparkSession.builder().getOrCreate().emptyDataFrame
@@ -22,4 +25,7 @@ object SparkOpInstance8 extends SparkOp {
   override def runConfigurations: RunConfigurations = {
     new RunConfigurations()
   }
+  override def namespace: Namespace = Namespace.Legacy
+  override def visibility: SparkOpVisibility = SparkOpVisibility.AllDomains
+  override def sharing: DatasetSharing = DatasetSharing.Private
 }
