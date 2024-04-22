477e69032333910a4e50f69a2dc44c729d067dfe
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-22 20:04:53 +0000

    Correct return type of inputs method in SparkOpInstance1 to match SubdomainOp trait

--- subdomains/domain-A/src/main/scala/sparkops/SparkOpInstance1.scala
+++ subdomains/domain-A/src/main/scala/sparkops/SparkOpInstance1.scala
@@ -1,14 +1,14 @@
 package subdomains.domainA.sparkops
 import org.apache.spark.sql.SparkSession
 
-import platform.common_classes.{Metadata, RunConfigurations, SubdomainOp}
+import platform.common_classes.{Metadata, RunConfigurations, SubdomainOp, Input}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SparkOpInstance1 extends SubdomainOp {
   val randomValue: Int = 7345 // Hardcoded random value
 
   override def name: String = "dataset/spark-op-instance-1"
-  override def inputs: Set[String] = Set() // No inputs, making it a root
+  override def inputs: Set[Input] = Set() // No inputs, making it a root
   override def query(inputs: Map[String, DataFrame]): DataFrame = {
     // Removed usage of SparkOpInstance20.randomValue due to submodule isolation rules
     // If needed, replace with a hardcoded value specific to SparkOpInstance1
