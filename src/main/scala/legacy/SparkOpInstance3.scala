f1ad81b47c612707ae8d5173f72ed83266912c77
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-23 19:48:28 +0000

    Adapt SparkOpInstance3 to extend SubdomainOp trait and update inputs method

--- src/main/scala/legacy/SparkOpInstance3.scala
+++ src/main/scala/legacy/SparkOpInstance3.scala
@@ -1,18 +1,16 @@
 package legacy
 import org.apache.spark.sql.SparkSession
 
-import platform.common_classes.SparkOp
+import platform.common_classes.{SubdomainOp, Metadata, RunConfigurations, Input}
 import org.apache.spark.sql.DataFrame
-import platform.common_classes.Metadata
-import platform.common_classes.RunConfigurations
 
-object SparkOpInstance3 extends SparkOp {
+object SparkOpInstance3 extends SubdomainOp {
   val randomValue: Int = 6243 // Hardcoded random value
 
   override def name: String = "dataset/spark-op-instance-3"
   // Updated to hardcoded name to comply with submodule isolation rules
-  // Removed incorrect hardcoded input name
-  override def inputs: Set[String] = Set() 
+  // Adapted to SubdomainOp trait
+  override def inputs: Set[Input] = Set() 
   override def query(inputs: Map[String, DataFrame]): DataFrame = {
     // Removed usage of SparkOpInstance2.randomValue due to submodule isolation rules
     // If needed, replace with a hardcoded value specific to SparkOpInstance3
