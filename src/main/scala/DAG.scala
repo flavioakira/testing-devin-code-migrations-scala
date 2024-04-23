04832fc19baf58bdb106a7ad21a0a29550862c72
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-23 19:52:58 +0000

    Update DAG.scala to separate SparkOp and SubdomainOp instances

--- src/main/scala/DAG.scala
+++ src/main/scala/DAG.scala
@@ -1,12 +1,12 @@
 package legacy
 
-import platform.common_classes.SparkOp
+import platform.common_classes.{SparkOp, SubdomainOp}
 
 object DAG {
   val ops: Set[SparkOp] = Set(
     // legacy.SparkOpInstance1, // Removed reference to SparkOpInstance1 as it has been migrated to a new submodule
     // legacy.SparkOpInstance2, // Removed reference to SparkOpInstance2 as it has been migrated to a new submodule
-    SparkOpInstance3,
+    // SparkOpInstance3, // Now extends SubdomainOp, not included in SparkOp set
     SparkOpInstance4,
     SparkOpInstance5,
     SparkOpInstance6,
@@ -24,5 +24,10 @@ object DAG {
     SparkOpInstance18,
     SparkOpInstance19,
     SparkOpInstance20
+  )
+
+  val subdomainOps: Set[SubdomainOp] = Set(
+    // Include instances of SubdomainOp here
+    SparkOpInstance3 // Adapted to extend SubdomainOp
   )
 }
