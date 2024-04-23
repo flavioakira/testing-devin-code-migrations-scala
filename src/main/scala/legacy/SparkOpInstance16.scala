ba83567ba2fd52bf03b6938e762868780f821d12
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-23 20:58:57 +0000

    Update SparkOpInstance16 inputs to comply with submodule isolation rules

--- src/main/scala/legacy/SparkOpInstance16.scala
+++ src/main/scala/legacy/SparkOpInstance16.scala
@@ -1,13 +1,13 @@
 package legacy
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
-import platform.common_classes.{SparkOp, Metadata, RunConfigurations}
+import platform.common_classes.{SubdomainOp, Metadata, RunConfigurations, Input}
 
-object SparkOpInstance16 extends SparkOp {
+object SparkOpInstance16 extends SubdomainOp {
   val randomValue: Int = 6729 // Hardcoded random value
 
   override def name: String = "dataset/spark-op-instance-16"
-  override def inputs: Set[String] = Set(SparkOpInstance15.name) // Reference to SparkOpInstance15 as an input using object name
+  override def inputs: Set[Input] = Set() // Updated to comply with SubdomainOp trait
   override def query(inputs: Map[String, DataFrame]): DataFrame = {
     SparkSession.builder().getOrCreate().emptyDataFrame
   }
